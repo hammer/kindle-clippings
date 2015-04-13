339771bcaef7635214b71a905125a61458ac4581
Jeff Hammerbacher <jeff.hammerbacher@gmail.com>
2015-04-13 01:07:43 -0400

    Checkpointing something broken for the night.

--- parse_kindle_clippings.ml
+++ parse_kindle_clippings.ml
@@ -1,11 +1,9 @@
 #! /usr/bin/env ocaml -w -8
 #use "topfind"
 #thread
+#require "str"
 #require "core"
 open Core.Std
-
-(* TODO(hammer): parse from the command line *)
-let clippings_file = ""
 
 type note = {
     title : string;
@@ -14,13 +12,21 @@ type note = {
     text : string;
   }
 
+let location_regex = Str.regexp "Loc\\. \\([0-9]+\\)"
+
+let parse_location_n location_s =
+  let _ = Str.search_forward location_regex location_s 0 in
+  Str.matched_group 1 location_s
+
 (* TODO(hammer): handle bad notes *)
+(* TODO(hammer): get location regex working with Core *)
 let process_note note =
   let title :: rest = note in
   let location_and_added :: text_parts = rest in
-  let location :: added :: _ = String.split ~on:'|' location_and_added in
+  let location_s :: added :: _ = String.split ~on:'|' location_and_added in
+  let location_n = parse_location_n location_s in
   let text = String.concat text_parts in
-  { title; location; added; text }
+  { title; location = location_n; added; text }
 
 let rec process_lines lines =
   let note, lines = List.split_while ~f:((<>) "==========") lines in
@@ -29,5 +35,9 @@ let rec process_lines lines =
   | _ -> (process_note note) :: (process_lines (List.drop lines 1))
 
 let () =
-  let notes = process_lines (In_channel.read_lines clippings_file) in
-  List.iter ~f:(fun note -> Format.printf "%s\n" note.text) notes
+  (* TODO(hammer): use array manipulation utilities from Core *)
+  for i = 1 to Array.length Sys.argv - 1 do
+    let clippings_file = Sys.argv.(i) in
+    let notes = process_lines (In_channel.read_lines clippings_file) in
+    List.iter ~f:(fun note -> Format.printf "%s|%s|%s\n" note.title note.location note.text) notes
+  done
